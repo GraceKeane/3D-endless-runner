e5f28e298c57a5f86bdce0ee905e2897b9513833
gracekeane <gracekeane82@gmail.com>
2020-12-08 16:10:42 +0000

    Lives depleting correctly

--- Assets/__Scripts/StartButton.cs
+++ Assets/__Scripts/StartButton.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class StartButton : MonoBehaviour
 {
+    int maxLives = 4;
+
     // Start icon navigates to game scene
     public void Changemenuscene(string scenename)
     {
+        PlayerPrefs.SetInt("lives", maxLives);
         Application.LoadLevel(scenename);
     }
 }
