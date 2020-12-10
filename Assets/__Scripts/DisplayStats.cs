5e3765d8b47094fbd555b156a36165d9e8e07fd4
gracekeane <gracekeane82@gmail.com>
2020-12-10 19:08:04 +0000

    Stats fully working

---
+++ Assets/__Scripts/DisplayStats.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayStats : MonoBehaviour
+{
+    public Text lastScore;
+    public Text highestScore;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        if(PlayerPrefs.HasKey("lastscore"))
+        {
+            lastScore.text = "Last Score: " + PlayerPrefs.GetInt("lastscore");
+        } else {
+            lastScore.text = "Last Score: 0";
+        }
+
+        if(PlayerPrefs.HasKey("highscore"))
+        {
+            highestScore.text = "Highest Score: " + PlayerPrefs.GetInt("highscore");
+        } else {
+            highestScore.text = "Highest Score: 0";
+        }
+
+
+    }
+
+}
